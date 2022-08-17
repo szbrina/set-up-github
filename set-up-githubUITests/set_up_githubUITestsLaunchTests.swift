3d9bf742b794781505df72dfeb5b8b60f2d45deb
Sabrina Zhang <szcamino@gmail.com>
2022-08-17 07:21:45 -0700

    Initial Commit

---
+++ set-up-githubUITests/set_up_githubUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  set_up_githubUITestsLaunchTests.swift
+//  set-up-githubUITests
+//
+//  Created by Sabrina on 8/17/22.
+//
+
+import XCTest
+
+class set_up_githubUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
